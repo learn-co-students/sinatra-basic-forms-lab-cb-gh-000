64989d69f47e06d67415b06f8cc511790f8e19e8
robertjosephwayne <robertjwayne@gmail.com>
2017-04-27 22:57:48 -0400

    Add TODO items

--- app.rb
+++ app.rb
@@ -1,5 +1,10 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  # TODO - Build out a GET request to load a home page. The home page should go to the main route /.
+  # TODO - The home page will also need a new view index.erb. This page should welcome you to the Puppy Adoption Site. Add this view to the controller action.
 
+  # TODO - Set up another controller action for a user to be able to view this form in the browser.
+
+  # TODO - third controller action that takes the input from the user and renders a third view (views/display_puppy.erb) which displays the info for the puppy that was just created.
 end
---
+++ models/puppy.rb
@@ -0,0 +1,4 @@
+# TODO - Build out a puppy class in models/puppy.rb.
+# TODO - Puppies should have name, breed, and age attributes.
+# TODO - Need to be able to pass these three attributes to initialization.
+# TODO - Need readers and writers for the attributes.
